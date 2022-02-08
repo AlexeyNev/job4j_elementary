e312e5cec397f40648b56af8d8f0a369e75218ad
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-02-08 10:11:50 +0400

    Checkstyle [#4123]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //ошибка в названии класса с маленькой буквы
         System.out.println("08.02.2022");
     }
 }
