3a8e144e811e91e14e06333fec6acbbdd3c6c627
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-02-08 16:02:37 +0400

    Арифметические операции. [#185]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String[] args) { //ошибка в названии класса с маленькой буквы
+    public static void main(String[] args) {
         System.out.println("08.02.2022");
     }
 }
