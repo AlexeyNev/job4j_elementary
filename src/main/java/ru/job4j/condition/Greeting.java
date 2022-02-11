cd2d5456af6371287288b1d7ab50f9ebf014b8d7
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-02-11 10:14:56 +0400

    String [#237407]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,18 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-//        String idea = "I like Java!";
-//        System.out.println(idea);
-//        idea = "I like Java!" + "But I am a newbie.";
-//        int year = 2022;
-//        idea = "I like Java!" + "But I am a newbie." + year;
-//        System.out.println(idea);
-        long l = 129;
-        byte b = (byte) l;
-        System.out.println(b); //poterya
-        float f = 12358.7f;
-        int i = 45981;
-        short rsl = (short) (f + i);
-        System.out.println(rsl); //poterya
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie.";
+        int year = 2022;
+        idea = idea + idea + year;
+        System.out.println(idea);
     }
 }
