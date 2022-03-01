7f0a7aab2920a42d8d269a0c9e17a80c09d98cb1
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-01 12:52:15 +0400

    [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -19,7 +19,12 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
 
-    }
+        float in1 = 140;
+        float expected1 = 2;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 rubles are 2. Test result : " + passed1);
 
+    }
 }
 
