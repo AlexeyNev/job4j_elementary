8e055e1ae3ed00e9df523f915d44a3c1e68273e8
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-03 17:48:45 +0400

    [#3611]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -15,43 +15,47 @@ public class X2Test {
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
+
     @Test
     public void whenA10B0C0X2Then40One() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected = a * x * x + b * x + c;
+        int expected = 3;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
+
     @Test
     public void whenA10B0C0X2Then40Two() {
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected = a * x * x + b * x + c;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
+
     @Test
     public void whenA10B0C0X2Then40Three() {
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = a * x * x + b * x + c;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
+
     @Test
     public void whenA10B0C0X2Then40Four() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
-        int expected = a * x * x + b * x + c;
+        int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
