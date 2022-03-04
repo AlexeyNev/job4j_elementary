0cb933ddca09f794ee9aaff997fefd4dfda7bd45
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-04 10:02:36 +0400

    [#3611]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -14,6 +14,7 @@ public class PointTest {
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
+
     @Test
     public void when00to30then2() {
         double expected = 2;
@@ -24,6 +25,7 @@ public class PointTest {
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
+
     @Test
     public void when00to40then2() {
         double expected = 4;
@@ -34,6 +36,7 @@ public class PointTest {
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
+
     @Test
     public void when00to50then2() {
         double expected = 3.6;
