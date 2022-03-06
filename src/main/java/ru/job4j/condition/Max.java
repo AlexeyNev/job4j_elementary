683bfbe91128684729cecb8911fc18f1b44d2504
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-06 21:19:54 +0400

    [#189 #272099]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,26 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
-    }
-
-    public static int oneMax(int left, int right) {
-        int result = left < right ? right : left;
-        return result;
-    }
-
-    public static int equals(int left, int right) {
-        int result = left == right ? right : left;
-        return result;
+        return left >= right ? left : right;
     }
 
     public static void main(String[] args) {
         int s = Max.max(2, 1);
         System.out.println(s);
-        int d = Max.oneMax(1, 2);
-        System.out.println(d);
-        int f = Max.equals(2, 4);
-        System.out.println(f);
     }
 }
