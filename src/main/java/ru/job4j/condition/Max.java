0856af3d949cf1408f7117388995cd046e836c66
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-06 21:21:43 +0400

    [#189 #272

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,6 +5,7 @@ public class Max {
         return left >= right ? left : right;
     }
 
+
     public static void main(String[] args) {
         int s = Max.max(2, 1);
         System.out.println(s);
