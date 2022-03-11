a914f90b702741203d160ff3af4c398056acdf04
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-11 20:46:31 +0400

    [#159464]

---
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if (first >= second && first >= third) {
+            result = first;
+        }
+        if (second >= first && second >= third) {
+            result = second;
+        }
+        if (third >= first && third >= second) {
+            result = third;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ThreeMax.max(10, 5, 1));
+        System.out.println(ThreeMax.max(10, 50, 1));
+        System.out.println(ThreeMax.max(1, 5, 100));
+        System.out.println(ThreeMax.max(1, 1, 1));
+        System.out.println(ThreeMax.max(10, 10, 1));
+        System.out.println(ThreeMax.max(100, 1, 100));
+        System.out.println(ThreeMax.max(1, 100, 100));
+    }
+}
