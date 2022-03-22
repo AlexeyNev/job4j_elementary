313e1715764a13338f271cac89f9c690e9ac96ea
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-03-22 07:30:30 +0400

    [#156313 #278267]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -3,13 +3,11 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
-            month = ((ivan *= 3) / nik);
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
         }
-        return month + 1;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Fitness.calc(95, 90));
+        return month;
     }
 }
