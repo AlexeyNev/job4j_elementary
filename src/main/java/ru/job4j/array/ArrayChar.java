21fc18e26e7b8bb295f95181a89f11752afad9e2
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-04 08:14:29 +0400

    [#41585 #283662]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,14 +3,11 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
-        for (int i = 0; i < word.length; i++) {
-            System.out.println(i);
-        }
         for (int i = 0; i < pref.length; i++) {
-            if (word[i] == pref[i]) {
-                return true;
+            if (word[i] != pref[i]) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
