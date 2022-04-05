d9e04ab7d848cb6d4321efdac6c3e2f561958532
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-05 15:14:13 +0400

    [#304228 #284111]

--- src/main/java/ru/job4j/array/EqLast.java
+++ src/main/java/ru/job4j/array/EqLast.java
@@ -2,9 +2,6 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        for (int i = 0; i < right.length; i++) {
-            System.out.println(i);
-        }
         return left[left.length - 1] == right[right.length - 1];
     }
 }
