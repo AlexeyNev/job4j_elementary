bcf36669d1071ebce785a3ae9f91dba9dbdd4c5a
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-15 15:50:23 +0400

    2.7

---
+++ src/main/java/ru/job4j/array/MergeRows.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class MergeRows {
+    public static int[] merge(int[][] data) {
+        if (null == data) return null;
+        int size = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (null != data[i]) size += data[i].length;
+        }
+        int[] res = new int[size];
+        for (int i = 0, j = 0; i < data.length; i++) {
+            if (null != data[i] && data[i].length > 0) {
+                System.arraycopy(data[i], 0, res, j, data[i].length);
+                j += data[i].length;
+            }
+        }
+        return res;
+    }
+}
