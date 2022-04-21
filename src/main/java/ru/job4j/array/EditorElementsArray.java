d353cda2c1a6d01b2135ca954b1a2a497109acfa
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-21 15:41:44 +0400

    3.7

--- src/main/java/ru/job4j/array/EditorElementsArray.java
+++ src/main/java/ru/job4j/array/EditorElementsArray.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class EditorElementsArray {
     public static int[][] changeData(int[][] array, int el) {
-        int count = 0;
 
         for (int i = 0; i < array.length; i++) {
             for (int f = 0; f < array[i].length; f++) {
                 if (array[i][f] > el) {
                     array[i][f] = array[i][f] - el;
-                    count++;
                 } else {
                     array[i][f] = 0;
                 }
