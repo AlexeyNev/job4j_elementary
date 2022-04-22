c9e67d77c023414dbdae06c7c587262e19756849
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-22 15:30:56 +0400

    4.4

--- src/main/java/ru/job4j/array/Initials.java
+++ src/main/java/ru/job4j/array/Initials.java
@@ -1,11 +1,11 @@
-package ru.job4j.array;
-
-public class Initials {
-    public static String convert(String[] fio) {
-
-        String pattern = "(\\S+\\s)(\\S{1})\\S+\\s(\\S{1})\\S+";
-        String str = fio.replaceAll(pattern, "$1$2.$3.");
-        System.out.println(str);
-        return str;
-    }
-}
+//package ru.job4j.array;
+//
+//public class Initials {
+//    public static String convert(String[] fio) {
+//
+//        String pattern = "(\\S+\\s)(\\S{1})\\S+\\s(\\S{1})\\S+";
+//        String str = fio.replaceAll(pattern, "$1$2.$3.");
+//        System.out.println(str);
+//        return str;
+//    }
+//}
---
+++ src/main/java/ru/job4j/array/NumberToArray.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class NumberToArray {
+    public static int[] resolve(int number) {
+
+    }
+}
