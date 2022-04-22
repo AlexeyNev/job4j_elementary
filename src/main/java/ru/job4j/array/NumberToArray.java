3d93c2fb6ea8b5dd849c034942ee538fcf8c8d6f
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-22 15:40:40 +0400

    4.5

--- src/main/java/ru/job4j/array/NumberToArray.java
+++ src/main/java/ru/job4j/array/NumberToArray.java
@@ -1,9 +1,13 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class NumberToArray {
     public static int[] resolve(int number) {
+        int[] rsl = new int[number];
 
+        for (int i = 0; i < number; i++) {
+            rsl[i] = number % 10;
+            number /= 10;
+        }
+        return rsl;
     }
 }
