6dd4d638ddfa8eb94fb326e7834e6ced4a5915ef
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-04-27 19:22:39 +0400

    1.5

---
+++ src/main/java/ru/job4j/loop/Hamming.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Hamming {
+    public static int checkStrings(String left, String right) {
+        int i = 0, count = 0;
+        while (i < left.length()) {
+            if (left.charAt(i) != right.charAt(i))
+                count++;
+            i++;
+        }
+        return count;
+    }
+}
