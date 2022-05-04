eb2a3a6363f046561a5ce99888d4c30a45f202bc
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-05-04 18:24:53 +0400

    -

--- src/main/java/ru/job4j/array/Transposition.java
+++ src/main/java/ru/job4j/array/Transposition.java
@@ -4,10 +4,13 @@ public class Transposition {
     public static int[][] convert(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] result = new int[m][n];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                result[i][j] = n * i + j;
+        int[][] result = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < m; j++) {
+                int temp = result[i][j];
+                result[i][j] = result[j][i];
+                result[j][i] = temp;
             }
         }
         return result;
